373d46bff831cb9a179c88d1c38dab5ffd60c5c5
Jaciel Dias <jaciel.dias@gmail.com>
2025-07-16 07:48:18 -0300

    Corrigindo calculo bonus gerente

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -7,7 +7,7 @@ public class Gerente extends Empregado{
     @Override
     public double calcularBonus(Departamento departamento) {
         if(departamento.alcancouMeta()) {
-            return (getSalarioFixo() * 0.2) + (departamento.getValorAtingidoMeta() - departamento.getValorMeta() * 0.01); 
+            return getSalarioFixo() * 0.2 + (departamento.getValorAtingidoMeta() - departamento.getValorMeta()) * 0.01; 
         } else {
             return 0;
         }
