7176d169d51324045cc082ae3334bde49c8a6598
Jaciel Dias <jaciel.dias@gmail.com>
2025-08-22 06:55:11 -0300

    Fix Exception task 0

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -2,8 +2,9 @@
 public class Numero {
 	
 	public static void dividir(int a, int b) {
+		int resultado = 0;
 		try {
-			int resultado = a/b;
+			resultado = a/b;
 		} catch (Exception e) {
 			System.out.println("Nao eh possivel dividir por zero");
 			System.out.println(String.format("%d / %d = %d", a, b, resultado));
