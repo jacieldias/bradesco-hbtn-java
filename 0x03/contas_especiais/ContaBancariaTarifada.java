25bf4df8ec6e9e042972aa9639af8b4e5fd9f767
Jaciel Dias <jaciel.dias@gmail.com>
2025-08-22 06:51:33 -0300

    fix

--- 0x03/contas_especiais/ContaBancariaTarifada.java
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -3,6 +3,10 @@ public class ContaBancariaTarifada extends ContaBancariaBasica{
 
     private int quantidadeTransacoes;
 
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual){
+        super(numeracao, taxaJurosAnual);
+    }
+
     @Override
     public void sacar(double valor) throws OperacaoInvalidaException{
         super.sacar(valor);
