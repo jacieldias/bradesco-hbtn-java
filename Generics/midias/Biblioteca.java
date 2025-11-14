72ea7832e0e1821427d14b5a09b0987bc989f4e0
Jaciel Dias <jaciel.dias@gmail.com>
2025-11-14 17:38:41 +0000

    opc generics

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,18 @@
+package Generics.midias;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca<T extends Midia> {
+
+    private List<T> midias = new ArrayList<>();
+
+    public void adicionarMidia(T midia){
+        midias.add(midia);
+    }
+
+    public List<T> obterListaMidias(){
+        return midias;
+    }
+
+}
---
+++ Generics/midias/Livro.java
@@ -0,0 +1,15 @@
+package Generics.midias;
+
+public class Livro extends Midia{
+    private int edicao;
+
+    public Livro(String nome, int edicao) {
+        super(nome);
+        this.edicao = edicao;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+}
